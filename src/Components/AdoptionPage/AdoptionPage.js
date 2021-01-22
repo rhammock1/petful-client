b939b881537aa8f806f7fcfac6e1541fb86c31a0
rhammock1 <rhamm1027@gmail.com>
2021-01-22 16:21:18 -0500

    skeleton of adoption page worked out

--- src/Components/AdoptionPage/AdoptionPage.js
+++ src/Components/AdoptionPage/AdoptionPage.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import PetContext from '../../PetContext';
 
 class AdoptionPage extends React.Component {
+
+  static contextType = PetContext;
+
   render() {
+    const {
+      topPets,
+      Person,
+      thankYou
+    } = this.context;
     return (
       <div className='page-container'>
         <div className='page-heading'>
@@ -9,6 +18,24 @@ class AdoptionPage extends React.Component {
         </div>
         <div className='adoption-container'>
           {/* Add context then make another container for each animal.  */}
+          {Object.entries(topPets).map((pet, i) => {
+            console.log(pet)
+            pet = pet[1];
+            return (
+              <div key={i} className='pet-container'>
+                <img src={pet.imageURL} alt={pet.imageDescription} />
+                <span>{pet.imageDescription}</span>
+                <div className='details'>
+                  <ul>
+                    <li>{pet.name}, Age {pet.age}</li>
+                    <li>{pet.sex} - {pet.breed}</li>
+                    <li>{pet.story}</li>
+                  </ul>
+                </div>
+                <button type='button'>Adopt Me</button>
+              </div>
+            )
+          })}
         </div>
       </div>
       )
